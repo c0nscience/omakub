2e40b4311664220eb703215a1a3fa576ba5d1bb2
Benjamin Herzig <ber.bherzig@pm.me>
2025-09-04 21:03:10 +0200

    fix 1password installation

--- install/terminal/setup-1password-ssh.sh
+++ install/terminal/setup-1password-ssh.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+mkdir -p ~/.config/1Password/ssh
 cp ~/.local/share/omakub/configs/ssh/agent.toml ~/.config/1Password/ssh/
+
+mkdir ~/.ssh
 cp ~/.local/share/omakub/configs/ssh/config ~/.ssh/
