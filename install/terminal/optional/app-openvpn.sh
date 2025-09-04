9058b31c32ba07f5d28dff9a5516ae067b97aeea
Benjamin Herzig <benjamin.herzig@dilax.com>
2025-09-04 15:41:57 +0200

    more revinements
    add open vpn aliases
    add installation of rust apps directly to have a newer version
    clean up things we don't need

--- install/terminal/optional/app-openvpn.sh
+++ install/terminal/optional/app-openvpn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt install -y apt-transport-https curl
-sudo sh -c 'curl -sSfL https://packages.openvpn.net/packages-repo.gpg >/etc/apt/keyrings/openvpn.asc'
-sudo sh -c 'echo "deb [signed-by=/etc/apt/keyrings/openvpn.asc] https://packages.openvpn.net/openvpn3/debian noble main" >>/etc/apt/sources.list.d/openvpn3.list'
+sudo apt install -y apt-transport-https
+curl -sSfL https://packages.openvpn.net/packages-repo.gpg | sudo tee /etc/apt/keyrings/openvpn.asc
+echo "deb [signed-by=/etc/apt/keyrings/openvpn.asc] https://packages.openvpn.net/openvpn3/debian noble main" | sudo tee /etc/apt/sources.list.d/openvpn3.list
 sudo apt update -y
 sudo apt install -y openvpn3
