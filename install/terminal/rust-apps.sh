12a683c928da34a83ad4eae519605424ff6870ce
Benjamin Herzig <benjamin.herzig@dilax.com>
2025-09-04 15:41:57 +0200

    more revinements
    add open vpn aliases
    add installation of rust apps directly to have a newer version
    clean up things we don't need

---
+++ install/terminal/rust-apps.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# install cargo-binstall to speedup rust installations
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+
+# install rust tools
+cargo binstall -y -q xh ripgrep bat eza zoxide fd-find
