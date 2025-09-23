84a387362b300af6661114d1b54069fb99ead747
Benjamin Herzig <benjamin.herzig@dilax.com>
2025-09-23 09:44:34 +0200

    add missing rust tools

--- install/terminal/optional/app-oxidise.sh
+++ install/terminal/optional/app-oxidise.sh
@@ -4,5 +4,7 @@
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
 # install rust tools
-cargo binstall -y -q xh ripgrep bat eza zoxide fd-find
+cargo binstall -y -q xh ripgrep bat eza zoxide fd-find mprocs bacon
 cargo install -y --locked ncspot tree-sitter-cli
+
+rustup component add rust-analyzer
