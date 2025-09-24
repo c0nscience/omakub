796e93cd426676bf09ed50060d18812fc4719fd0
Benjamin Herzig <benjamin.herzig@dilax.com>
2025-09-24 08:49:15 +0200

    change undotree key

--- configs/neovim/undotree.lua
+++ configs/neovim/undotree.lua
@@ -8,7 +8,7 @@ return {
 			-- your options
 		},
 		keys = { -- load the plugin only when using it's keybinding:
-			{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+			{ "<leader>U", "<cmd>lua require('undotree').toggle()<cr>", desc = "Toggle Undotree" },
 		},
 	},
 }
