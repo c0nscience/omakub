fbc1c5e2f02e48294e2bcd26517a5a65d81275c4
Benjamin Herzig <ber.bherzig@pm.me>
2025-10-01 13:56:24 +0200

    create local docker network to attach everything to it

--- install/terminal/select-dev-storage.sh
+++ install/terminal/select-dev-storage.sh
@@ -9,19 +9,20 @@ else
 fi
 
 if [[ -n "$dbs" ]]; then
+  sudo docker network create --driver bridge local
   for db in $dbs; do
     case $db in
     MySQL)
-      sudo docker run -d --restart unless-stopped -p "3306:3306" --name=mysql -e MYSQL_ROOT_PASSWORD= -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:9.4
+      sudo docker run -d --network local --restart unless-stopped -p "3306:3306" --name=mysql -e MYSQL_ROOT_PASSWORD= -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:9.4
       ;;
     Redis)
-      sudo docker run -d --restart unless-stopped -p "6379:6379" --name=redis redis:8
+      sudo docker run -d --network local --restart unless-stopped -p "6379:6379" --name=redis redis:8
       ;;
     PostgreSQL)
-      sudo docker run -d --restart unless-stopped -p "5432:5432" --name=postgres -e POSTGRES_HOST_AUTH_METHOD=trust postgres:17
+      sudo docker run -d --network local --restart unless-stopped -p "5432:5432" --name=postgres17 -e POSTGRES_HOST_AUTH_METHOD=trust postgres:17
       ;;
     Mail)
-      sudo docker run -d --restart unless-stopped -p "5000:80" -p "2525:25" --name=mail rnwood/smtp4dev
+      sudo docker run -d --network local --restart unless-stopped -p "5000:80" -p "2525:25" --name=mail rnwood/smtp4dev
       ;;
     esac
   done
