3786d80f126681d963b97644183736fddaebcedf
Benjamin Herzig <benjamin.herzig@dilax.com>
2025-11-07 13:44:23 +0100

    add puppet config

--- install/terminal/app-neovim.sh
+++ install/terminal/app-neovim.sh
@@ -44,6 +44,7 @@ if [ ! -d "$HOME/.config/nvim" ]; then
   cp ~/.local/share/omakub/configs/neovim/neo-tree.lua ~/.config/nvim/lua/plugins/
   cp ~/.local/share/omakub/configs/neovim/undotree.lua ~/.config/nvim/lua/plugins/
   cp ~/.local/share/omakub/configs/neovim/mason.lua ~/.config/nvim/lua/plugins/
+  cp ~/.local/share/omakub/configs/neovim/puppet.lua ~/.config/nvim/lua/plugins/
 fi
 
 # Replace desktop launcher with one running inside Alacritty
