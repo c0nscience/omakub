fae5cd04721b6fb89fa284dc2d602a367bb80ee1
Benjamin Herzig <ber.bherzig@pm.me>
2025-11-14 20:54:13 +0100

    add sqlx

--- install/terminal/optional/app-oxidise.sh
+++ install/terminal/optional/app-oxidise.sh
@@ -4,7 +4,7 @@
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
 # install rust tools
-cargo binstall -q xh ripgrep bat eza zoxide fd-find mprocs bacon tokei
+cargo binstall -q xh ripgrep bat eza zoxide fd-find mprocs bacon tokei sqlx-cli
 cargo install --locked ncspot tree-sitter-cli
 
 rustup component add rust-analyzer
