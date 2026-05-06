c293f208c4d4de64a72f2d941d7f7b6a5fdf3e69
Benjamin Herzig <ber.bherzig@pm.me>
2026-05-06 13:57:28 +0200

    update tools

--- install/terminal/optional/app-oxidise.sh
+++ install/terminal/optional/app-oxidise.sh
@@ -4,7 +4,9 @@
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
 # install rust tools
-cargo binstall -y -q xh ripgrep bat eza zoxide fd-find mprocs bacon tokei sqlx-cli
+cargo binstall -y -q xh ripgrep bat eza zoxide fd-find mprocs bacon tokei sqlx-cli yazi-fm yazi-cli
 cargo install --locked ncspot tree-sitter-cli
 
 rustup component add rust-analyzer
+
+mise use -g fzf@latest
