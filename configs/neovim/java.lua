a2594a6c89e4100d10c2e914e039b5fa70cd4c70
Benjamin Herzig <benjamin.herzig@dilax.com>
2026-05-07 13:56:03 +0200

    fix java

--- configs/neovim/java.lua
+++ configs/neovim/java.lua
@@ -1,21 +1,23 @@
 return {
-  "mfussenegger/nvim-jdtls",
-  opts = {
-    settings = {
-      java = {
-        configuration = {
-          runtimes = {
-            {
-              name = "JavaSE-17",
-              path = vim.fn.expand("~/.local/share/mise/installs/java/zulu-17"),
-            },
-            {
-              name = "JavaSE-21",
-              path = vim.fn.expand("~/.local/share/mise/installs/java/zulu-21"),
-            },
-          },
-        },
-      },
-    },
-  },
+	"mfussenegger/nvim-jdtls",
+	opts = {
+		settings = {
+			java = {
+				contentProvider = { preferred = "fernflower" },
+				maven = { downloadSources = true },
+				configuration = {
+					runtimes = {
+						{
+							name = "JavaSE-17",
+							path = vim.fn.expand("~/.local/share/mise/installs/java/zulu-17"),
+						},
+						{
+							name = "JavaSE-21",
+							path = vim.fn.expand("~/.local/share/mise/installs/java/zulu-21"),
+						},
+					},
+				},
+			},
+		},
+	},
 }
