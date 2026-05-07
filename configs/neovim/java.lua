2c72ee44f97b7db0ed19484c21a1a0531a5383cc
Benjamin Herzig <benjamin.herzig@dilax.com>
2026-05-07 11:57:36 +0200

    fix java

--- configs/neovim/java.lua
+++ configs/neovim/java.lua
@@ -1,9 +1,6 @@
 return {
   "mfussenegger/nvim-jdtls",
   opts = {
-    -- The java binary that runs jdtls itself
-    cmd = { vim.fn.expand("~/.local/share/mise/installs/java/zulu-21/bin/java") },
-
     settings = {
       java = {
         configuration = {
