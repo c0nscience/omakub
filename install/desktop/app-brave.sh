166dee4e9267f84caa6ec1679ab3bc7bfc4749cc
Benjamin Herzig <ber.bherzig@pm.me>
2026-08-17 09:07:10 +0200

    update brave install script

--- install/desktop/app-brave.sh
+++ install/desktop/app-brave.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
 sudo apt update -y
 sudo apt install -y brave-browser
 xdg-settings set default-web-browser brave-browser.desktop
